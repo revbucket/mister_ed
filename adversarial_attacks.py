9cbadfab8abd7b52da35c84fc37914f69c1ac18d
revbucket <mattjordan.mail@gmail.com>
2018-04-10 18:23:41 -0500

    added alternating PGD

--- adversarial_attacks.py
+++ adversarial_attacks.py
@@ -402,6 +402,101 @@ class LInfPGD(AdversarialAttack):
 
 ############################################################################
 #                                                                          #
+#                           Alternating PGD                                #
+#                                                                          #
+############################################################################
+class AltPGD(LInfPGD):
+
+    def __init__(self, classifier_net, normalizer, to_loss_fxn, fro_loss_fxn,
+                 use_gpu=False):
+        """ HACKETY HACK:
+
+        For some reason, my regularization isn't working the way I want it to.
+        What I'll do instead is a hokey-pokey game:
+        I have two alternating losses and I try to minimize them in alternating
+        iterations.
+
+        to_loss refers to a loss that we want to MAXIMIZE (xentropy, say)
+        fro_loss refers to a loss that we want to MINIMIZE (lpips, say)
+        """
+        super(AltPGD, self).__init__(classifier_net, normalizer,
+                                     use_gpu=use_gpu)
+        self.to_loss_fxn = to_loss_fxn
+        self.fro_loss_fxn = fro_loss_fxn
+        self.losses = [self.to_loss_fxn, self.fro_loss_fxn]
+
+
+    def attack(self, examples, labels, l_inf_bound=8.0/255.0,
+               step_size=1.0/255.0, fro_scale=0.9, num_iterations=20,
+               random_init=True, signed=True, verbose=True):
+
+    assert 0.0 <= l_inf_bound <= 1.0
+
+    if not verbose:
+        self.validator = lambda ex, label, iter_no: None
+    else:
+        self.validator = self.validation_loop
+
+
+    self.classifier_net.eval()
+    var_examples = Variable(examples, requires_grad=True)
+    var_labels = Variable(labels, requires_grad=False)
+
+    [loss.setup_attack_batch(var_examples.clone()) for loss in self.losses]
+
+    ##########################################################################
+    #   Build adversarial examples                                           #
+    ##########################################################################
+
+    intermed_images = var_examples
+
+    if random_init:
+        var_examples = self._random_init(var_examples, l_inf_bound)
+        self.validator(var_examples, var_labels, iter_no="RANDOM")
+
+    # start iterating
+    for iter_no in xrange(num_iterations):
+        # Reset gradients
+        [loss.zero_grad() for loss in self.losses()]
+
+        # to step
+        to_loss = self.to_loss_fxn(intermed_images, var_labels)
+
+        torch.autograd.backward(to_loss)
+
+        signs = torch.sign(intermed_images.grad.data) * step_size
+        clamp_inf = utils.clamp_ref(intermed_images.data + signs, examples,
+                                    l_inf_bound)
+        clamp_box = torch.clamp(clamp_inf, 0., 1.)
+
+        intermed_images = Variable(clamp_box, requires_grad=True)
+        [loss.zero_grad() for loss in self.losses()]
+
+        # fro step
+
+        fro_loss = self.fro_loss_fxn(intermed_images, var_labels)
+        torch.autograd.backward(fro_loss)
+
+        signs = torch.sign(intermed_images.grad.data) * step_size * fro_scale
+        clamp_inf = utils.clamp_ref(intermed_images.data - signs, examples,
+                                    l_inf_bound)
+        clamp_box = torch.clamp(clamp_inf, 0., 1.0)
+
+        intermed_images = Variable(clamp_box, requires_grad=True)
+        self.validator(intermed_images, var_labels, iter_no=iter_no)
+
+
+    return intermed_images.data
+
+
+
+
+
+
+
+
+############################################################################
+#                                                                          #
 #                           Carlini Wagner attacks                         #
 #                                                                          #
 ############################################################################
